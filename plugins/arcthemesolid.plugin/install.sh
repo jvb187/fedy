8901016d455f88a4d6d2da53b921581ef32152f2
jvb187 <jvanbortel@gmail.com>
2016-06-30 13:57:22 -0400

    Update install.sh

--- plugins/arcthemesolid.plugin/install.sh
+++ plugins/arcthemesolid.plugin/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")/home:Horst3180.repo
+dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")_standard/home:Horst3180.repo
 dnf -y install arc-theme-solid
